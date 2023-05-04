392f2f41c3ed406848589cd0f905e1a604ff66a6
Yennifer Pernia <perniayennifer@gmail.com>
2023-05-04 15:40:02 -0400

    update css Persona information

--- src/components/home/PersonalInformation.js
+++ src/components/home/PersonalInformation.js
@@ -1,12 +1,12 @@
 import React from "react";
 import content_img from "../../assets/imgs/content_img.png";
-import hexagono from "../../assets/imgs/hexagono.png";
-import foto_carnet from "../../assets/imgs/foto_carnet.jpeg";
-import puntos from "../../assets/imgs/puntos.png";
+import hexagon from "../../assets/imgs/hexagono.png";
+import photo_carnet from "../../assets/imgs/foto_carnet.jpeg";
+import points from "../../assets/imgs/puntos.png";
 
 export const PersonalInformation = () => {
   return (
-    <div className="personal-information__content">
+    <div className="personal-information">
       <div className="personal-information__content-hexagon">
         <div className="personal-information__content-name">
           <div className="personal-information__last-name">
@@ -19,32 +19,29 @@ export const PersonalInformation = () => {
         <div className="personal-information__content-img">
           <img
             className="personal-information__img"
-            src={hexagono}
+            src={hexagon}
             alt="content_img"
           />
           <img
-            className="personal-information__img_two"
-            src={foto_carnet}
-            alt="foto_carnet"
+            className="personal-information__photo-carnet"
+            src={photo_carnet}
+            alt="photo_carnet"
           />
           <img
-            className="personal-information__img_three"
-            src={puntos}
-            alt="foto_carnet"
+            className="personal-information__img-points"
+            src={points}
+            alt="photo_carnet"
           />
         </div>
 
         <div className="personal-information__content-text">
-          <div className="personal-information__is">
+          <div className="personal-information__profession">
             <p>FRONTEND</p>
             <p>ENGINEER</p>
           </div>
-          <div className="personal-information__business">
-            <p>INGENIERÍA EN SISTEMAS O.G.R. S.A</p>
-            <div className="personal-information__icono-location">
-              <i className="fas fa-map-marker-alt "></i>
-              <p>Chile</p>
-            </div>
+          <div className="personal-information__icon-location">
+            <i className="fas fa-map-marker-alt "></i>
+            <p>Chile</p>
           </div>
         </div>
       </div>
