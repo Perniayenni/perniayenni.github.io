2cd7d5439710b8bc6d1aae112dd51d73c38f0f66
Yennifer Pernia <yennipernia@MacBook-Air-de-Yennifer.local>
2023-06-24 15:31:36 -0400

    feat: probando Cname

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -3,7 +3,7 @@ export const certifications = [
     'img': 'https://drive.google.com/uc?export=download&id=154Ghm7W1jBeRYD4-gMn5kr5CY-n3WC75',
     'expedition': 2017,
     'company': 'Udemy',
-    'url': 'https://www.udemy.com/certificate/UC-96fa0ee1-1c1e-48ad-87a3-35d561defbea/'
+    'url': 'https://www.udemy.com/certificate/UC-2N4S87WT/'
   },
   {
     'img': 'https://drive.google.com/uc?export=download&id=1zMbUo16MHs9GtgnkH9a-K-bzCLR2Ddko',
